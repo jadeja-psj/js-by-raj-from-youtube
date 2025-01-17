1e120f683d0add1f20323f817213da1865da262b
jadeja-psj <fastuse194@gmail.com>
2025-01-17 14:24:40 +0000

    object in javaScript

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,6 +10,7 @@ const dc_heros = ["superman","flash","batman"]
 // console.log(all_heros);
 // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
 
+// concate two arrays using spread or spreading two arrays
 // const all_new_arrays = [...marvel_heros,...dc_heros]
 // console.log(all_new_arrays)
 // the above will also give the same output as concat method
@@ -18,6 +19,20 @@ const another_array = [1,2,3,[4,5,6],7,8,[9,0,[1,2,3]]]
 // if there is situation as shown in the another_array then to resolve it you can use. flat() method
 
 const real_another_array = another_array.flat(2)
-console.log(real_another_array);
+// console.log(real_another_array);
 
+// To check whether the given is arrya or not
+// console.log(Array.isArray("Hello")) // false
+
+
+// Now if i want to convert the above "Hello string to array then use .from(content)"
+// console.log(Array.from("Hello"));
+
+
+// convert the variables into array
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
 
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+// singleton will not formed when we use literals and it will be formed when we use constructor
+
+// object literals
+const Jsuser = {
+    name:"fastboy",
+    age:10,
+    location:"Delhi",
+    email:"hello@google.com",
+    isLoggedIn : false,
+    lastLoginDays: ["Monday","Tuesday","wednesday"]
+}
+
+// There are two ways to access the 
