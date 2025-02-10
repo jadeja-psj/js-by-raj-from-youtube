4b056d41cc96d88ef076bef7553ddca1403507a4
jadeja-psj <fastuse194@gmail.com>
2025-02-10 14:11:01 +0000

    completed Api Not Promises started

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,2 @@
+// A Promise is an object representing the eventual completion or failure of an asynchronous operation. Since most people are consumers of already-created promises, this guide will explain consumption of returned promises before explaining how to create them.
+
