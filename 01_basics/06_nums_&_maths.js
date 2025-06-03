164e273b92b0ee23c9d314f9854abbf4667f012b
jadeja-psj <fastuse194@gmail.com>
2025-06-03 04:41:51 +0000

    complete Number and Maths

--- 01_basics/06_nums_&_maths.js
+++ 01_basics/06_nums_&_maths.js
@@ -1,51 +1,41 @@
 const score = 400
-// console.log(score) // It automatically detects that the given score is in number 
-// output:400
-
+// console.log(score);
 
 const balance = new Number(100)
-// console.log(balance) // it will specifically cast th value in number and tell us that it is a number
-// output: [Number: 100] it is casted specially in number
+// console.log(balance);
 
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
-// Try the above code in the console and you will get the methods on Number 
-
-// Some of its properties are:
-//console.log(balance.toString().length) //convert the num to string and print the length of the string
-// console.log(balance.toFixed(2))
-// It will print only 2 numbers after a decimal point
-
-const otherNumber = 23.8966
-
-//console.log(otherNumber.toPrecision(3)); //precise for 3 and other will be round off
-//the values should be between 1-21 to get the precisie value
+// const otherNumber = 23.8966
+// const otherNumber = 123.8966
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(3)); // it gives the number of digits mentions in .toPrecision(n) 
 
 const hundreds = 1000000
-//console.log(hundreds.toLocaleString('en-IN')) // 10,00,000
-// By default it use us standard, so Indian std 'en-IN'
+// console.log(hundreds.toLocaleString()); // default value is based on US standards.
 
-// ***********************Maths**********************
+// console.log(hundreds.toLocaleString('en-IN')); // This is indian standards  
 
-// console.log(Math); //Object [Math] {}
-// it is object and it also has many methods that you can see on console
-// some methods
-// console.log(Math.abs(-4)); // 4
-// abs -> absolute value it converts -ve value to +ve
-// +ve will not be changed
-// console.log(Math.round(4.6))//5 it will round of the num
-// console.log(Math.ceil(4.2))//ceil->roundup only to upper value //5
-// console.log(Math.floor(4.9))//floor->roundup only to the lesser value // 4
-// console.log(Math.min(4,3,0,5,6)) // 0
-// console.log(Math.max(4,3,0,5,6)) // 6
 
-console.log(Math.random()); // it will give the values between 0 to 1 by default
+// ++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
 
-console.log((Math.random()*10) + 1); // min value will be 1
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5324));
+// console.log(Math.floor(4.9)); // it will choose the nearest smallest value
+// console.log(Math.ceil(4.2)); // it will choose the nearest larger value
+
+// console.log(Math.min(4,5,7,9,3,0.1));
+// console.log(Math.max(4,5,7,9,3,0.1));
+
+// console.log(Math.random()); // it will give the value between the range of 0-1
+// console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
 
-// another value
+
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random()*(max - min + 1)) + min)
-
+console.log(Math.floor(Math.random() * ((max - min) + 1)) + min)
