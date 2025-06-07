39cfe57d86a4f3259f46422030fa11ebf55cb783
jadeja-psj <fastuse194@gmail.com>
2025-06-07 06:27:41 +0000

    complete loop

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -1,6 +1,6 @@
 // using .reduce value
 
-const myNum = [1, 2, 3];
+// const myNum = [1, 2, 3];
 
 // let myTotal = myNum.reduce(function (acc,currVal){
 //     console.log(`acc: ${acc} and currval: ${currVal}`);
@@ -17,25 +17,129 @@ const myNum = [1, 2, 3];
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-const shoppingCart = [
-  {
-    itemName: "js course",
-    price: 2999,
-  },
-  {
-    itemName: "py course",
-    price: 999,
-  },
-  {
-    itemName: "mobile dev course",
-    price: 5999,
-  },
-  {
-    itemName: "data science course",
-    price: 12999,
-  },
-];
+// const shoppingCart = [
+//   {
+//     itemName: "js course",
+//     price: 2999,
+//   },
+//   {
+//     itemName: "py course",
+//     price: 999,
+//   },
+//   {
+//     itemName: "mobile dev course",
+//     price: 5999,
+//   },
+//   {
+//     itemName: "data science course",
+//     price: 12999,
+//   },
+// ];
 
-const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+// const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
 
-console.log(priceToPay);
+// console.log(priceToPay);
+
+// ************************* Practice *******************************
+
+const arr = [1,2,3,4]
+
+// for(const i of arr){
+//   console.log(i);
+  
+// }
+
+
+const greetings = "Hello world!"
+// for(const greet of greetings)
+// {
+//   console.log(`Each char is ${greet}`);
+  
+// }
+
+
+// Maps
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA',"United States of Amaerica")
+// map.set('Fr',"France")
+// map.set('IN',"India")
+
+// console.log(map);
+
+// for(const [key, value] of map)
+// {
+  // console.log(key, ':-', value );
+  
+// }
+
+
+const myObject  = {
+  js:'javascript',
+  cpp:'C++',
+  rd:"ruby",
+  swift:"swift by apple"
+}
+
+for(const key in myObject)
+{
+  // console.log(myObject[key]);
+  // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js","rd","py","java","cpp"]
+
+for(const key in programming){
+  // console.log(key);
+  // console.log(programming[key]);
+  
+}
+
+
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach(function (item){
+  // console.log(item);
+  
+})
+
+// coding.forEach((val) => {
+//   console.log(val);
+// })
+
+function printMe(item){
+  // console.log(item);
+}
+
+coding.forEach(printMe)
+
+
+coding.forEach((item, index, arr)=>{
+  // console.log(item,index,arr);
+  
+})
+
+
+const myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js"
+  },
+  {
+    languageName: "java",
+    languageFileName: "java"
+  },
+  {
+    languageName: "python",
+    languageFileName: "py"
+  }
+]
+
+
+myCoding.forEach( (item) => {
+  console.log(item.languageName);
+  
+})
+
+
--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -16,7 +16,7 @@
 // To solve the above problem we will use an another method filter
 // filter returns the value
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
 // }) //[ 5, 6, 7, 8, 9, 10 ]
@@ -36,27 +36,27 @@ myNums.forEach( (num) =>{
 // console.log(newNums);
 
 // *******************************************************************
-const books = [
-  { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
-  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
-  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
-  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
-  { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
-  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
-  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
-  { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
-  { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
-];
+// const books = [
+//   { title: "Book One", genre: "Fiction", publish: 1981, edition: 2004 },
+//   { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+//   { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+//   { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+//   { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+//   { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+//   { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+//   { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+//   { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+// ];
 
-let userBooks = books.filter((bk) => bk.publish >= 1992);
+// let userBooks = books.filter((bk) => bk.publish >= 1992);
 
-userBooks = books.filter((bk) => bk.edition <= 2000);
+// userBooks = books.filter((bk) => bk.edition <= 2000);
 // console.log(userBooks);
 
 // *******************************************************************
 // ***************************************************************** ********************************************************************  ++++++++++++++++++++++++++++++MAP+++++++++++++++++++++++++++++++++++
 
-const MyNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const MyNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // let NewNums  = myNums.map((num) => num+10)
 // console.log(NewNums);
 
@@ -64,10 +64,27 @@ const MyNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // In JavaScript, chaining refers to the technique of calling multiple methods on the same object in a single statement. It allows you to "chain" method calls together, improving code readability and reducing redundancy.
 
-let NewNums = MyNums
-                    .map((num) => num * 10)
-                    .map((num) => num + 1)
-                    .filter((num) => num >= 40);
+// let NewNums = MyNums
+//                     .map((num) => num * 10)
+//                     .map((num) => num + 1)
+//                     .filter((num) => num >= 40);
 
-console.log(NewNums);
+// console.log(NewNums);
 
+
+// ***************** Practice ************************************
+
+// let myArray = ["flash", "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+
+let score = 1;
+do{
+  console.log(`Score is ${score}`);
+  score++
+}while(score <= 10);
