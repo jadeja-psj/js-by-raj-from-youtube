d1701b7fdb68c73e440aa538663301caa613f475
jadeja-psj <fastuse194@gmail.com>
2025-06-13 10:04:00 +0000

    complete promises

--- 09_advance_one/PromisesN.js
+++ 09_advance_one/PromisesN.js
@@ -77,6 +77,41 @@ const promiseFive = new Promise((resolve, reject)=>{
 })
 
 
-async function consumePromiseFive({
+async function consumePromiseFive(params) {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
     
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data  = await response.json()
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log("E: ", error);
+        
+//     }
+// }
+
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
 })
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
